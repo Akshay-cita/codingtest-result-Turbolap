41bbb81b201a5bc0cc2fcd25d1c8adc486a95d08
Akshay Narayanan <38578142+Akshay-cita@users.noreply.github.com>
2020-10-12 12:07:43 +0530

    Add files via upload
    Answers

---
+++ answer_2.py
@@ -0,0 +1,21 @@
+
+
+
+input_string=str(input("Enter the alphabet\n"))
+list=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','v','W','X','Y','Z']
+if input_string.islower():
+	input_string=input_string.upper()
+	if len(input_string)%2==0:
+		split1=input_string[:len(input_string)//2]
+		split2=input_string[len(input_string)//2:]
+		split1.upper()
+		split2.upper()
+	
+
+
+
+	
+
+
+
+
